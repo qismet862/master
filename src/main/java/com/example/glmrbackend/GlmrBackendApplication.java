47efce543feb606824fd01838770054e1642b47d
qismet <qismetqedirli5@gmail.com>
2021-10-14 21:17:38 +0400

    the first structure of the project was established

---
+++ src/main/java/com/example/glmrbackend/GlmrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.glmrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlmrBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glmrbackend/GlmrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glmrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
